c0a49239a0889c26d75eb40b68eb8f9579767939
crissu91 <cristinaf.stefan@gmail.com>
2023-10-17 16:15:50 +0100

    add jest logo to the tech task component

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,4 +1,4 @@
-import react from  "../assets/React-logo.png";
+import react from "../assets/React-logo.png";
 import node from "../assets/Node-logo.png";
 import javascript from "../assets/JavaScript-logo.png";
 import typescript from "../assets/TypeScript-logo.png";
@@ -10,17 +10,41 @@ import css from "../assets/CSS-logo.png";
 import tailwind from "../assets/tailwind-logo.png";
 import svelte from "../assets/svelte-logo.png";
 import supabase from "../assets/supabase-logo.png";
+import jest from "../assets/jest-logo.png";
 
 function TechStack() {
-    const logos = [react, node, javascript, typescript, postgresql, sql, express, html, css, tailwind, svelte, supabase]
-    return (
-        <div>
-            <h1 className="flex items-center justify-center text-4xl font-medium mb-6 text-gray-700 pt-20">Tech Stack</h1>
-            <div className="grid grid-cols-3 lg:grid-cols-5 place-items-center p-4">
-                {logos.map(logo => <img className="max-w-24 max-h-20 p-2 duration-300 hover:scale-125" src={logo} alt={logo} key={logo} />)}          
-            </div>
-        </div>
-    )
+  const logos = [
+    react,
+    node,
+    javascript,
+    typescript,
+    postgresql,
+    sql,
+    express,
+    html,
+    css,
+    tailwind,
+    svelte,
+    supabase,
+    jest,
+  ];
+  return (
+    <div>
+      <h1 className="flex items-center justify-center text-4xl font-medium mb-6 text-gray-700 pt-20">
+        Tech Stack
+      </h1>
+      <div className="grid grid-cols-3 lg:grid-cols-5 place-items-center p-4">
+        {logos.map((logo) => (
+          <img
+            className="max-w-24 max-h-20 p-2 duration-300 hover:scale-125"
+            src={logo}
+            alt={logo}
+            key={logo}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default TechStack;
